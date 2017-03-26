f2ab70a3a4a7ab78687ec99ee9fac223b5242ed1
Jason Rebello <rebellojason7@gmail.com>
2017-03-26 01:58:37 -0400

    added suitesparse to installation

--- install_coc.sh
+++ install_coc.sh
@@ -3,22 +3,25 @@
 set -e # exit on first error
 
 install_camodocal(){
-    echo "Installing BLAS"
+    echo "Installing BLAS --------------------------------------"
     sudo apt-get install -y libblas-dev
 
-    echo "Installing Boost"
+    echo "Installing Boost -------------------------------------"
     sudo apt-get install -y libboost-all-dev
     
-    echo "Installing Eigen3"
+    echo "Installing Eigen3 ------------------------------------"
     sudo apt-get install -y libeigen3-dev
     
-    echo "Installing Glog"
+    echo "Installing Glog --------------------------------------"
     sudo apt-get install -y libgoogle-glog-dev
     sudo apt-get install -y libgoogle-glog-doc
     sudo apt-get install -y libgoogle-glog0v5
 
-    echo "Installing Cuda"
+    echo "Installing Cuda --------------------------------------"
     sudo apt-get install -y nvidia-cuda-toolkit
+
+    echo "Installing SuiteSparse -------------------------------"
+    sudo apt-get install -y libsuitesparse-dev
 }
 
 # MAIN
